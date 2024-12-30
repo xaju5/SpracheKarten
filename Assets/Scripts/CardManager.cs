85b7c65a75939ee263e590fe8bb4e51ea83874b6
Xabier <xajubdev@gmail.com>
2024-12-31 00:01:02 +0100

    Flip and Slide card

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -10,16 +10,24 @@ public class CardManager : MonoBehaviour
 {
     private CSVController cSVController;
     private List<Verb> verbList;
+    [SerializeField] private GameObject canvas;
     [SerializeField] private TextMeshProUGUI title;
     [SerializeField] private TextMeshProUGUI word;
-    [SerializeField] private TextMeshProUGUI TopicToGuess;
+    [SerializeField] private TextMeshProUGUI topicToGuess;
     [SerializeField] private float flipSpeed;
+    [SerializeField] private float slideSpeed;
 
-    private int index;
-    private bool enableFlip;
+    private int cardIndex;
     private bool isFacingFront;
+    private bool enableFlip;
+    private bool flipDirection;
+    private bool changedFlipDirection;
+    private int flipCounter;
+    private Quaternion targetRotation;
+    private bool enableSlide;
+    private GameObject oldcanvas;
 
-    void  Awake() {
+    void Awake() {
         cSVController = new CSVController();
         verbList = cSVController.ReadCSV();
     }
@@ -27,57 +35,102 @@ public class CardManager : MonoBehaviour
     void Start()
     {
         isFacingFront = true;
-        index = 1;
+        cardIndex = 1;
         title.text = verbList[0].infiniv;
-        word.text = verbList[index].infiniv;
-        TopicToGuess.text = verbList[0].translation + "?";
+        word.text = verbList[cardIndex].infiniv;
+        topicToGuess.text = verbList[0].translation + "?";
+        ResetFlipBool();
+        enableSlide = false;
     }
 
 
     void Update() {
-        if(Input.GetKeyDown(KeyCode.Return))
-            GetNextIndex();
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Return) && !enableSlide)
+            SetNextCard();
+        if(Input.GetKeyDown(KeyCode.Space) && !enableFlip)
             FlipCard();
-        FlipCardAnimation();
+        
+        if(enableFlip) FlipCardAnimation();
+        if(enableSlide) SlideCardAnimation();
     }
 
-    private void GetNextIndex(){
-        index++;
-        if (index == 0) index = 1;
-        if (index >= verbList.Count) index = 1;
 
-        isFacingFront = true;
-        title.text = verbList[0].infiniv;
-        word.text = verbList[index].infiniv;
-        TopicToGuess.text = verbList[0].translation + "?";
-    }
-
+/// FLIP CARD
     private void FlipCard(){
+        enableFlip = true;
+    }
+    private void FlipCardAnimation(){
+        if(transform.rotation == new Quaternion(0,0,0,1)) changedFlipDirection = true;
+        if(transform.rotation == new Quaternion(-0.707106829f,0,0,0.707106829f)) changedFlipDirection = true;
+
+        if(changedFlipDirection){
+            flipDirection = !flipDirection;
+            flipCounter++;
+            targetRotation = flipDirection ? new Quaternion(-0.707106829f,0,0,0.707106829f) : new Quaternion(0,0,0,1);
+            changedFlipDirection = false;
+            
+            if(flipCounter == 2 ){
+                FlipCardText();
+            } 
+
+            if(flipCounter == 3 ){
+                ResetFlipBool();
+                return;
+            } 
+        }
+        if(flipCounter == 1 || flipCounter == 2)
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, flipSpeed * Time.deltaTime);
+    }
+    private void ResetFlipBool(){
+        flipCounter = 0;
+        changedFlipDirection = false;
+        flipDirection = false;
+        enableFlip = false;
+    }
+    private void FlipCardText(){
 
         if(isFacingFront){
             //TODO: No pasar verbos sino caras estandarizadas
             isFacingFront = false;
-            title.text = verbList[0].translation;
-            word.text = verbList[index].translation;
-            TopicToGuess.text = verbList[0].infiniv + "?";
+            title.text = verbList[0].translation + ":";
+            word.text = verbList[cardIndex].translation;
+            topicToGuess.text = verbList[0].infiniv + "?";
         }
         else{
             isFacingFront = true;
-            title.text = verbList[0].infiniv;
-            word.text = verbList[index].infiniv;
-            TopicToGuess.text = verbList[0].translation + "?";
+            title.text = verbList[0].infiniv + ":";
+            word.text = verbList[cardIndex].infiniv;
+            topicToGuess.text = verbList[0].translation + "?";
         }
 
-        //FlipCardAnimation();
     }
 
+/// NEXT CARD
+    private void SetNextCard(){
+        oldcanvas = Instantiate(gameObject);
+        Destroy(oldcanvas.GetComponent<CardManager>());
+        enableSlide = true;
 
-    private void FlipCardAnimation(){
-        if(transform.rotation == new Quaternion(0,0,0,1)) enableFlip = true;
-        if(transform.rotation == new Quaternion(-0.707106829f,0,0,0.707106829f)) enableFlip = false;
+        GetNextIndex();
+        isFacingFront = true;
+        title.text = verbList[0].infiniv;
+        word.text = verbList[cardIndex].infiniv;
+        topicToGuess.text = verbList[0].translation + "?";
+    }
 
-        Quaternion targetRotation = enableFlip ? new Quaternion(-0.707106829f,0,0,0.707106829f) : new Quaternion(0,0,0,1);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, flipSpeed * Time.deltaTime);
+    private void GetNextIndex(){
+        cardIndex++;
+        if (cardIndex == 0) cardIndex = 1;
+        if (cardIndex >= verbList.Count) cardIndex = 1;
+    }
+
+    private void SlideCardAnimation(){
+        Vector3 targetPosition = new Vector3(16,0,0);
+        oldcanvas.transform.position =  Vector3.MoveTowards(oldcanvas.transform.position, targetPosition , slideSpeed * Time.deltaTime);
+
+        if(oldcanvas.transform.position == targetPosition){
+            Destroy(oldcanvas);
+            enableSlide = false;
+        }
     }
 }
